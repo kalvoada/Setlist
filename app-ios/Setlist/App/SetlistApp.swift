9a498707337fafc675741c096bc1bc9d977080f5
kalvoada <kalvoada@fit.cvut.cz>
2026-02-05 19:06:24 +0100

    Initial commit

---
+++ app-ios/Setlist/App/SetlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetlistApp.swift
+//  Setlist
+//
+//  Created by Adam Kalvoda on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SetlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app-ios/Setlist/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Setlist
+//
+//  Created by Adam Kalvoda on 05.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
